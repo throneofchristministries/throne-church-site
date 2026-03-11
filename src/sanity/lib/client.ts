3d4f1280b6e28f9eaf572544de814cebc3c6893f
Victoria <leadscommunityschool7@gmail.com>
2026-03-11 20:22:07 +0000

    export sanity client

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from 'next-sanity';
 
-// 1. Updated Sanity Client Configuration
-const client = createClient({
-  projectId: "lhudqnko", // Corrected Project ID from your screenshot
+// 1. Add "export" here so other files can see it
+export const client = createClient({
+  projectId: "lhudqnko", 
   dataset: "production",
   apiVersion: "2024-03-11",
   useCdn: false,
